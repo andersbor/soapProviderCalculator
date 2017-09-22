b202d7ff3dbf701aaf324dc99b0307cad444d20f
andersbor <anbo@easj.dk>
2017-09-22 11:47:52 +0200

    Add + Add array

---
+++ SoapProviderCalculator/ICalculatorService.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+
+namespace SoapProviderCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculatorService" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+
+        [OperationContract]
+        double Add(double a, double b);
+
+        [OperationContract]
+        double Add(double[] numbers);
+    }
+
+
+
+}
