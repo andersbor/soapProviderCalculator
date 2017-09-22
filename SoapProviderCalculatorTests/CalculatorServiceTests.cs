1ffecf7521668849a78faf74f2c2f8cf771a338b
andersbor <anbo@easj.dk>
2017-09-22 12:09:41 +0200

    unit tests added

---
+++ SoapProviderCalculatorTests/CalculatorServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SoapProviderCalculator.Tests
+{
+    [TestClass]
+    public class CalculatorServiceTests
+    {
+        private readonly CalculatorService _obj = new CalculatorService();
+
+        [TestMethod]
+        public void AddTest()
+        {
+            double res = _obj.Add(3.2, 4.6);
+            Assert.AreEqual(7.8, res);
+        }
+
+        [TestMethod]
+        public void SubtractTest()
+        {
+            double res = _obj.Subtract(10.7, 4.6);
+            Assert.AreEqual(6.1, res);
+        }
+
+        [TestMethod]
+        public void SumTest()
+        {
+            double[] numbers = {1.1, 2.2, 3.3};
+            double res = _obj.Sum(numbers);
+            Assert.AreEqual(6.6, res);
+        }
+    }
+}
