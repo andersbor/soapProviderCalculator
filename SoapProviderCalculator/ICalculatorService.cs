a5a4feec64c1d65736b71bebebcb9d897980bd4e
andersbor <anbo@easj.dk>
2017-09-26 09:53:41 +0200

    better tests

--- SoapProviderCalculator/ICalculatorService.cs
+++ SoapProviderCalculator/ICalculatorService.cs
@@ -4,7 +4,7 @@ namespace SoapProviderCalculator
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculatorService" in both code and config file together.
     [ServiceContract]
-    public interface ICalculatorService
+    public interface ICalculatorService 
     {
         [OperationContract]
         double Add(double a, double b);
